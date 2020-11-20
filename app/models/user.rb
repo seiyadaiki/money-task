ddc7654fc3617d3b6980c97b8e1d5f91637caeeb
seiyadaiki <seiyadaiki@gmail.com>
2020-11-20 14:13:37 +0900

    indexview編集とテストコードgemインストール

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :used_monies
+  has_one :goal
+  with_options presence: true do
+    validates :nickname, length: { maximum: 40 }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)\w{6,12}\z/, message: 'is invalid. Input half-width characters.' }
+    validates :password_confirmation, confirmation: true
+    validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
+    validates :namekana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :birthday
+  end
 end
